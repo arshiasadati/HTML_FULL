9ff788edb887b304a9f1efcb4605bbb1618b2c4a
arshiasadati <arshiasadati1388@gmail.com>
2024-12-08 19:22:22 +0330

    update 8/12/2024

--- web-project2/client/src/components/CustomLayout.tsx
+++ web-project2/client/src/components/CustomLayout.tsx
@@ -1,8 +1,24 @@
+import logo from "../assets/—Pngtree—wolf logo_2306634.png"
 const CustomLayout = () => {
 
   return (
-    <div className="Main-body-home">
-      
+    <div className="main-body-home">
+      <div className="home-navbar-head">
+        <div className="logo-image-main-home-box">
+          <img src={logo} alt="" className="logo-image-main-home"/>
+          <p className="liongaming-text-main-header-home">
+            Lion Gaming
+          </p>
+        </div>
+      </div>
+      <div className="sidebar-home">
+        <a href="x">
+          فروشگاه
+        </a>
+        <a href="x">
+
+        </a>
+      </div>
     </div>
   );
 };
