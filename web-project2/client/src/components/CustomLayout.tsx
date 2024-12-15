38f3e9287a372439be18c3a0fa2e56b11580db6a
arshiasadati <arshiasadati1388@gmail.com>
2024-12-15 11:23:10 +0330

    update 15/12/2024

--- web-project2/client/src/components/CustomLayout.tsx
+++ web-project2/client/src/components/CustomLayout.tsx
@@ -4,21 +4,26 @@ const CustomLayout = () => {
   return (
     <div className="main-body-home">
       <div className="home-navbar-head">
+          <a href="x" className="home-navbar-head-registerlogin">
+            ورود / ثبت نام
+          </a>
+          <a href="x" className="home-navbar-head-store">
+             حمایت از ما
+          </a>
+          <a href="x" className="home-navbar-head-donate">
+             فروشگاه
+          </a>
+          <a href="" className="home-navbar-head-aboutus">
+             درباره ما
+          </a>
+          <a href="x" className="home-navbar-head-weblog">
+            وبلاگ
+          </a>
         <div className="logo-image-main-home-box">
           <img src={logo} alt="" className="logo-image-main-home"/>
-          <p className="liongaming-text-main-header-home">
-            Lion Gaming
-          </p>
         </div>
       </div>
-      <div className="sidebar-home">
-        <a href="x">
-          فروشگاه
-        </a>
-        <a href="x">
 
-        </a>
-      </div>
     </div>
   );
 };
